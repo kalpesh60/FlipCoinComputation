ba2e579789f387abdfe791c10076671f06cedec7
kalpesh60 <kalpeshdhalpe60@gmail.com>
2021-04-12 23:13:02 +0530

    added: singlet and calculated percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,14 +1,27 @@
 #!/bin/bash -x
 echo "welcome to flip coin computation problem"
 
+for (( i=1;i<=20;i++ ))
+do
+num=$(( $RANDOM % 2 ))
+  if [ $num -eq 0 ]
+    then
+        echo "Head"
+        let count1++
+  else
+        echo "Tail"
+        let count2++
+  fi
+done
 
-head=1
-tail=0
-num=1
-randomCheck=$(( RANDOM%2 ))
-if [ $randomCheck -eq $head ]
-then
-	echo "coin flips head"
-else
-	echo "coin flips tail"
-fi
+echo $count1
+echo $count2
+
+dict1[Head]="$count1"
+dict1[Tail]="$count2"
+
+percentage_of_head=$(( $count1 * 5 ))
+percentage_of_tail=$(( $count2 * 5 ))
+
+echo "Percentage_of_head : $percentage_of_head"
+echo "Percentage_of_tail : $percentage_of_tail"
