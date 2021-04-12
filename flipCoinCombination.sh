cb60512a41361617e2cb06532b3304c2cfe63c8a
kalpesh60 <kalpeshdhalpe60@gmail.com>
2021-04-12 23:07:30 +0530

    added:flipped the coin to get haid or tail

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,1 +1,14 @@
+#!/bin/bash -x
 echo "welcome to flip coin computation problem"
+
+
+head=1
+tail=0
+num=1
+randomCheck=$(( RANDOM%2 ))
+if [ $randomCheck -eq $head ]
+then
+	echo "coin flips head"
+else
+	echo "coin flips tail"
+fi
